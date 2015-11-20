8a063fc9ff2ac6fb9c4d8ce1c7624dc5854a591b
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-11-20 13:44:02 +0100

    Reorder routes, and get rid of Ember Data for now. It seems perfectly reasonable to pass the jmap-client objects as route models

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  jmap: Ember.inject.service(),
+  model() {
+    var request = this.get('jmap').client.getAccounts();
+
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      request.then(
+      function (accounts) {
+        resolve(accounts[0]);
+      }, reject);
+    });
+  }
+});
