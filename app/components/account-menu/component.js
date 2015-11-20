a3021fc234ca762c5c2a5724d29452d438aea0e3
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-11-20 13:44:02 +0100

    Reorder routes, and get rid of Ember Data for now. It seems perfectly reasonable to pass the jmap-client objects as route models

--- app/components/account-menu/component.js
+++ app/components/account-menu/component.js
@@ -1,12 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  store: Ember.inject.service(),
   session: Ember.inject.service(),
-
-  account: Ember.computed(function () {
-    return this.get('store').findRecord('account');
-  }),
 
   actions: {
     logout () {
--- app/models/account.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name:       DS.attr('string'),
-  isPrimary:  DS.attr('boolean'),
-  isReadOnly: DS.attr('boolean'),
-  hasMail:    DS.attr('boolean')
-});
